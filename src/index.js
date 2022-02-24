4c080e915b04cdeed19a8c9e641b2ea13dfa6e22
Teryo2 <96073567+Teryo2@users.noreply.github.com>
2022-02-24 20:40:30 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const MORSE_TABLE = {
+    '.-':     'a',
+    '-...':   'b',
+    '-.-.':   'c',
+    '-..':    'd',
+    '.':      'e',
+    '..-.':   'f',
+    '--.':    'g',
+    '....':   'h',
+    '..':     'i',
+    '.---':   'j',
+    '-.-':    'k',
+    '.-..':   'l',
+    '--':     'm',
+    '-.':     'n',
+    '---':    'o',
+    '.--.':   'p',
+    '--.-':   'q',
+    '.-.':    'r',
+    '...':    's',
+    '-':      't',
+    '..-':    'u',
+    '...-':   'v',
+    '.--':    'w',
+    '-..-':   'x',
+    '-.--':   'y',
+    '--..':   'z',
+    '.----':  '1',
+    '..---':  '2',
+    '...--':  '3',
+    '....-':  '4',
+    '.....':  '5',
+    '-....':  '6',
+    '--...':  '7',
+    '---..':  '8',
+    '----.':  '9',
+    '-----':  '0',
+    '**********': ' '
+};
+
+function decode(expr) {
+  const result = [];
+
+  for (let i = 0; i < expr.length; i += 10){
+    let symbols = expr.substr(i, 10);
+    let toMorseSymbols = symbols.replace(/00/g,'').replace(/10/g,'.').replace(/11/g,'-');
+
+    result.push(MORSE_TABLE[toMorseSymbols]);
+  }
+
+  return result.join('');
+}
+
+module.exports = {
+    decode
+}
